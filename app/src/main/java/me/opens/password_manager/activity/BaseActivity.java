e19f64c3ab6000cdc7fee8cac27b1bb6f38115ca
Prasun <prasun_1993@rediffmail.com>
2019-05-10 22:05:48 +0530

    Move to login activity from base activity automatically

--- app/src/main/java/me/opens/password_manager/activity/BaseActivity.java
+++ app/src/main/java/me/opens/password_manager/activity/BaseActivity.java
@@ -1,15 +1,36 @@
 package me.opens.password_manager.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import me.opens.password_manager.R;
 
 public class BaseActivity extends AppCompatActivity {
+
+
+    public static final String EXTRA_MESSAGE = "me.opens.password_manager.MESSAGE";
+    private static final String TAG = BaseActivity.class.getCanonicalName();
+    private static int TIME_OUT = 2500;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_base);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(BaseActivity.this, LoginActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Base Activity");
+                Log.i(TAG, "starting display login activity");
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }
