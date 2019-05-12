15bd011527403db1c21e06eacefb6be0c1d90ed8
Prasun <prasun_1993@rediffmail.com>
2019-05-12 13:11:16 +0530

    Prasun | Update script to generate apk

--- generate_signed_apk.sh
+++ generate_signed_apk.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Generating release apk ..."
-./gradlew assembleRelease -Pandroid.injected.signing.store.file=$KEYFILE -Pandroid.injected.signing.store.password=$STORE_PASSWORD -Pandroid.injected.signing.key.alias=$KEY_ALIAS -Pandroid.injected.signing.key.password=$KEY_PASSWORD
+./gradlew assembleRelease -Pandroid.injected.signing.store.file=$KEY_FILE -Pandroid.injected.signing.store.password=$KEY_PASSWORD -Pandroid.injected.signing.key.alias=$KEY_ALIAS -Pandroid.injected.signing.key.password=$KEY_PASSWORD
