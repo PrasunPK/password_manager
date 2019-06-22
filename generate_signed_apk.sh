18ecee67dd016ec4f299d3e67611183f66e3fffa
Prasun <prasun_1993@rediffmail.com>
2019-06-22 17:08:40 +0530

    Prasun | Update script

--- generate_signed_apk.sh
+++ generate_signed_apk.sh
@@ -2,3 +2,6 @@
 
 echo "Generating release apk ..."
 ./gradlew assembleRelease -Pandroid.injected.signing.store.file=$KEY_FILE -Pandroid.injected.signing.store.password=$KEY_PASSWORD -Pandroid.injected.signing.key.alias=$KEY_ALIAS -Pandroid.injected.signing.key.password=$KEY_PASSWORD
+
+echo "Moving to apk directory ..."
+mv ./app/build/outputs/apk/release/password-manager-1.1.1-release.apk ./release/apk/.
